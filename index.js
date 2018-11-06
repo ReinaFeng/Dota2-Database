c72d7d779cb1c16b1739d78e39f8b9d044cbfdf2
fegan104 <fegan104@gmail.com>
2018-11-05 22:45:18 -0500

    couple more improvements

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express')
+const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
+const http = require('http');
+const { Client } = require('pg');
+const { DATABASE_URL } = process.env;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/foo', (req, res) => res.send("Foo bar 🙂"))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const client = new Client({
+  connectionString: DATABASE_URL,
+  ssl:false
+});
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/match', (req, res) => {
+  client.connect()
+    .then(() => client.query('SELECT * FROM match;'))
+    .then((query) => {
+      console.log(query.rows)
+      res.end(`Result: ${query.rows}`);
+      client.end();
+    })
+    .catch(() => {
+      res.end("ERROR");
+      client.end();
+    });
+})
+
+app.listen(port, () => console.log(`Server running on ${port}/`))
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/foo', (req, res) => res.send("Foo bar 🙂"))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
