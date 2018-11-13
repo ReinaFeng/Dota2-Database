9b8267a6350db5ea26d486e48a49c30dd0c2814d
fegan104 <fegan104@gmail.com>
2018-11-12 23:27:12 -0500

    making progress

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const http = require('http');
+const asyncHandler = require('express-async-handler')
 const { Client } = require('pg');
 const { PORT, DATABASE_URL } = process.env;
 
 const client = new Client({
   connectionString: DATABASE_URL,
-  ssl:false
+  ssl: false
 });
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/match', (req, res) => {
-  client.connect()
-    .then(() => client.query('SELECT * FROM match;'))
-    .then((query) => {
-      console.log(query.rows)
-      res.end(`Result: ${query.rows}`);
-      client.end();
-    })
-    .catch(() => {
-      res.end("ERROR");
-      client.end();
-    });
-})
+const runQuery = async (queryString) => {
+  await client.connect()
+  const query = await client.query(queryString)
+  await client.end()
+  return query
+}
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/match', asyncHandler(async (req, res) => {
+  try {
+    const query = runQuery(`SELECT * FROM user_view1 WHERE steam_id=${req.params.userId};`)
+    console.log(query.rows)
+    res.end(`Result: ${query.rows}`);
+  } catch (error) {
+    res.end("ERROR");
+  }
+}))
+
+app.get('/users-win/:userId', asyncHandler(async (req, res) => {
+  try {
+    const query = runQuery(`SELECT * FROM user_view1 WHERE steam_id=${req.params.userId};`)
+    console.log(query.rows)
+    res.end(`Result: ${query.rows}`);
+  } catch (error) {
+    res.end("ERROR");
+  }
+}))
+
+app.get('/users-win', asyncHandler(async (req, res) => {
+  try {
+    const query = runQuery('SELECT * FROM user_view2;')
+    console.log(query.rows)
+    res.end(`Result: ${query.rows}`);
+  } catch (error) {
+    res.end("ERROR");
+  }
+}))
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}/`))
 
@@ -31,6 +58,6 @@ app.listen(PORT, () => console.log(`Server running on ${PORT}/`))
 //   .use(express.static(path.join(__dirname, 'public')))
 //   .set('views', path.join(__dirname, 'views'))
 //   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
+  // .get('/', (req, res) => res.render('pages/index', data: {}))
 //   .get('/foo', (req, res) => res.send("Foo bar 🙂"))
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
