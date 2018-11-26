ca27ff77b568ce127f6f7472b84f3b64df884ef1
fegan104 <fegan104@gmail.com>
2018-11-25 20:34:16 -0500

    added some data rendering

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const asyncHandler = require('express-async-handler')
 const { Client } = require('pg');
 const { PORT, DATABASE_URL } = process.env;
 
-const client = new Client({
-  connectionString: DATABASE_URL,
-  ssl: false
-});
-
 const runQuery = async (queryString) => {
+  const client = new Client({
+    connectionString: DATABASE_URL,
+    ssl: false
+  });
+  
   await client.connect()
   const query = await client.query(queryString)
   await client.end()
@@ -23,8 +23,22 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+/**************** UI PAGES *****************/
 app.get('/', (req, res) => res.render('pages/index'))
+app.get('/user_match', (req, res) => res.render('pages/user_match'))
+app.get('/user_overview', (req, res) => res.render('pages/user_overview'))
+app.get('/db', asyncHandler(async (req, res) => {
+  try {
+    const { rows } = await runQuery('SELECT * FROM hero_view1;')
+    res.render('pages/db', {results: rows})
+  } catch (error) {
+    console.error(error)
+    res.end(JSON.stringify(error));
+  }
+}))
 
+
+/**************** REST API *****************/
 app.get('/hero/1', asyncHandler(async (req, res) => {
   try {
     const { rows } = await runQuery('SELECT * FROM hero_view1;')
@@ -71,13 +85,5 @@ app.get('/users-win', asyncHandler(async (req, res) => {
     res.end(JSON.stringify(error));
   }
 }))
-
-app.get('/user_match', (req, res) => {
-  res.render('pages/user_match')
-})
-
-app.get('/user_overview', (req, res) => {
-  res.render('pages/user_overview')
-})
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`))
