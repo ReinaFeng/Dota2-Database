d79cf8bd5cc78955e8fb1a4b684937bd9a28857f
fegan104 <fegan104@gmail.com>
2018-11-25 19:13:27 -0500

    ui updates

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const url = require('url');
+const fs = require('fs');
 const asyncHandler = require('express-async-handler')
 const { Client } = require('pg');
 const { PORT, DATABASE_URL } = process.env;
@@ -23,42 +25,59 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('pages/index'))
 
-app.get('/hero', asyncHandler(async (req, res) => {
+app.get('/hero/1', asyncHandler(async (req, res) => {
   try {
-    const query = runQuery('SELECT * FROM hero_view1;')
-    console.log(query.rows)
-    res.end(`Result: ${query.rows}`);
+    const { rows } = await runQuery('SELECT * FROM hero_view1;')
+    res.end(JSON.stringify(rows));
   } catch (error) {
-    res.end("ERROR");
+    console.error(error)
+    res.end(JSON.stringify(error));
+  }
+}))
+
+app.get('/hero/2', asyncHandler(async (req, res) => {
+  try {
+    const { rows } = await runQuery('SELECT * FROM hero_view2;')
+    res.end(JSON.stringify(rows));
+  } catch (error) {
+    res.end(JSON.stringify(error));
+  }
+}))
+
+app.get('/users/favorite/:hero', asyncHandler(async (req, res) => {
+  try {
+    const { rows } = await runQuery('SELECT * FROM user_hero2;')
+    res.end(JSON.stringify(rows));
+  } catch (error) {
+    res.end(JSON.stringify(error));
   }
 }))
 
 app.get('/users-win/:userId', asyncHandler(async (req, res) => {
   try {
-    const query = runQuery(`SELECT * FROM user_view1 WHERE steam_id=${req.params.userId};`)
-    console.log(query.rows)
-    res.end(`Result: ${query.rows}`);
+    const { rows } = await runQuery(`SELECT * FROM user_view1 WHERE steam_id='${req.params.userId}';`)
+    res.end(JSON.stringify(rows));
   } catch (error) {
-    res.end("ERROR");
+    res.end(JSON.stringify(error));
   }
 }))
 
 app.get('/users-win', asyncHandler(async (req, res) => {
   try {
-    const query = runQuery('SELECT * FROM user_view2;')
-    console.log(query.rows)
-    res.end(`Result: ${query.rows}`);
+    const { rows } = await runQuery('SELECT * FROM user_view2;')
+    res.end(JSON.stringify(rows));
   } catch (error) {
-    res.end("ERROR");
+    console.error(error)
+    res.end(JSON.stringify(error));
   }
 }))
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}/`))
+app.get('/user_match', (req, res) => {
+  res.render('pages/user_match')
+})
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-  // .get('/', (req, res) => res.render('pages/index', data: {}))
-//   .get('/foo', (req, res) => res.send("Foo bar 🙂"))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/user_overview', (req, res) => {
+  res.render('pages/user_overview')
+})
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`))
