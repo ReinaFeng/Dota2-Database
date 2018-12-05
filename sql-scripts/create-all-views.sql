a486834723a6de3d4e677f48a4e6eea211290e03
fegan104 <fegan104@gmail.com>
2018-12-05 11:24:48 -0500

    ui finalized

--- sql-scripts/create-all-views.sql
+++ sql-scripts/create-all-views.sql
@@ -22,7 +22,7 @@ Order by win_rate
 -- View Hero By Win Rate
 -- This query will allow you to sort the Heroes by their win rate.
 CREATE OR REPLACE VIEW hero_view1 AS
-SELECT t2.name as hero_name, t1.win_rate
+SELECT t2.name as hero_name, t2.hero_id AS id, t1.win_rate
 FROM (
   SELECT 
     a.hero_id, cast((a.win_match) AS decimal(7,2))/cast((b.total_match) AS decimal(7,2)) as win_rate
@@ -44,25 +44,6 @@ FROM (
 ORDER BY win_rate
 ;
 
--- TODO this is wrong
--- View A User's Favorite Hero
--- This query will return the Hero that a given User uses most frequently in their Matches.
--- CREATE OR REPLACE VIEW user_hero2 AS
--- SELECT *
--- FROM(
--- 	SELECT 
---     steam_id, hero_id, count(*) as Times
--- 	FROM 
---     player
--- 	GROUP BY 
---     player.hero_id) as frq_hero, hero
--- WHERE 
---   hero.hero_id=frq_hero.hero_id
--- ORDER BY 
---   frq_hero.Times
--- ;
-
---TODO This is broken because user_item hase no steam_id
 -- View The Most Common Item for a Hero
 -- This query will tell you what Item is most commonly used by a given Hero.
 CREATE OR REPLACE VIEW item_hero AS
@@ -143,6 +124,7 @@ GROUP BY
 CREATE OR REPLACE VIEW all_hero_stats AS
 SELECT 
   hero.name as hero,
+  hero.hero_id as id,
   max(kills) as max_kill,
   avg(kills) as avg_kill,
   min(kills) as min_kill,
@@ -157,7 +139,7 @@ FROM
 WHERE 
   player.hero_id = hero.hero_id
 GROUP BY
-  hero.name
+  hero.name, hero.hero_id
 ;
 
 CREATE OR REPLACE VIEW user_hero_view1 AS
@@ -210,3 +192,29 @@ GROUP BY
   item_id, en_name
 ORDER BY 
   item_count DESC;
+
+-- find users favorite item:
+create or replace view user_favorite_item as
+select t3.steam_id,t4.en_name,count(*) as freqs
+from
+(select t1.steam_id,t2.item_id
+from (select steam_id,match_id,player_slot
+      from player) t1 left join
+     (select match_id,player_slot,item_id
+      from user_item) t2 on t1.match_id=t2.match_id and t1.player_slot=t2.player_slot) t3 left join
+(select item_id,en_name from item) t4 on t3.item_id=t4.item_id
+group by t3.steam_id,t4.item_id,t4.en_name
+order by t3.steam_id,freqs desc
+;
+
+-- # find users' teammates
+create or replace view teammates as
+select t1.steam_id,t2.steam_id as teammate,count(*) as freqs
+from (select steam_id,match_id
+      from player) t1 left join
+     (select match_id,steam_id
+      from player) t2 on t1.match_id=t2.match_id
+where t1.steam_id != t2.steam_id
+group by t1.steam_id,t2.steam_id
+order by freqs desc
+;
